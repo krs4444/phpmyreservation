6aa35309e25a6162eeba9d30c5e3c21a7fb3a380
Kristian Pinke <kristian.pinke@live.com>
2017-10-23 00:23:25 +0200

    - publishing info added - Control Panel translated

--- phpmyreservation/cp.php
+++ phpmyreservation/cp.php
@@ -1,4 +1,4 @@
-<?php 
+﻿<?php 
 
 include_once('main.php');
 
@@ -62,34 +62,34 @@ else
 
 ?>
 
-		<h3>User administration</h3>
+		<h3>Administrácia užívateľov</h3>
 
 		<div id="users_div"><?php echo list_users(); ?></div>
 
-		<p class="center_p"><input type="button" class="small_button blue_button" id="reset_user_password_button" value="Reset password"> <input type="button" class="small_button blue_button" id="change_user_permissions_button" value="Change permissions"> <input type="button" class="small_button" id="delete_user_reservations_button" value="Delete reservations"> <input type="button" class="small_button" id="delete_user_button" value="Delete user"></p>
+		<p class="center_p"><input type="button" class="small_button blue_button" id="reset_user_password_button" value="Znovu nastaviť heslo"> <input type="button" class="small_button blue_button" id="change_user_permissions_button" value="Zmeniť práva"> <input type="button" class="small_button" id="delete_user_reservations_button" value="Vymazať rezervácie"> <input type="button" class="small_button" id="delete_user_button" value="Vymazať užívateľa"></p>
 		<p class="center_p" id="user_administration_message_p"></p>
 
 		<hr>
 
-		<h3>Database administration</h3>
+		<h3>Administrácia databázy</h3>
 
-		<p class="smalltext_p">These will require a confirmation. Your user and reservations will not be deleted unless you delete everything.</p>
+		<p class="smalltext_p">Pre zmenu týchto nastavení, bude potrebné potvrdenie. Vaše údaje a rezervácie nebudú vymazané, pokým nezvolíte možnosť Vymazať všetko.</p>
 
-		<p><input type="button" class="small_button" id="delete_all_reservations_button" value="Delete all reservations"> <input type="button" class="small_button" id="delete_all_users_button" value="Delete all users"> <input type="button" class="small_button" id="delete_everything_button" value="Delete everything"></p>
+		<p><input type="button" class="small_button" id="delete_all_reservations_button" value="Vymazať všetky rezervácie"> <input type="button" class="small_button" id="delete_all_users_button" value="Vymazať všetkých užívateľov"> <input type="button" class="small_button" id="delete_everything_button" value="Vymazať všetko"></p>
 
 		<p id="database_administration_message_p"></p>
 
 		<hr>
 
-		<h3>System configuration</h3>
+		<h3>Systémové nastavenia</h3>
 
-		<p class="smalltext_p">Changing the price will not affect previous reservations.</p>
+		<p class="smalltext_p">Zmena ceny nezmení cenu predchádzajúcich rezervácií.</p>
 
 		<form action="." id="system_configuration_form"><p>
 
-		<input type="text" id="price_input" value="<?php echo get_configuration('price'); ?>"> <label for="price_input">Price per reservation, in <?php echo global_currency; ?></label><br><br>
+		<input type="text" id="price_input" value="<?php echo get_configuration('price'); ?>"> <label for="price_input">Cena rezervácie v <?php echo global_currency; ?></label><br><br>
 
-		<input type="submit" class="blue_button small_button" value="Save configuration">
+		<input type="submit" class="blue_button small_button" value="Uložiť nastavenia">
 
 		</p></form>
 
@@ -103,13 +103,13 @@ else
 
 ?>
 
-	<h3>Your usage</h3>
+	<h3>Vaša spotreba</h3>
 
-	<p class="smalltext_p">If you have used without making a reservation first, please click the button below. It can't be undone.</p>
+	<p class="smalltext_p">Ak ste použili saunu bez predchádzajúcej rezervácie, stlačte tlačidlo nižšie. Táto akcia sa nedá zvrátiť.</p>
 
 	<div id="usage_div"><?php echo get_usage(); ?></div>
 
-	<p><input type="button" class="blue_button small_button" id="add_one_reservation_button" value="Add 1 to my reservations"></p>
+	<p><input type="button" class="blue_button small_button" id="add_one_reservation_button" value="Pridaj 1 ku mojim rezerváciám"></p>
 
 	<p id="usage_message_p"></p>
 
@@ -122,9 +122,9 @@ else
 
 ?>
 
-	<h3>Your settings</h3>
+	<h3>Vaše nastavenia</h3>
 
-	<p class="smalltext_p">Before changing any setting, please verify that your details below are correct.</p>
+	<p class="smalltext_p">Predtým, ako zmeníte niektoré nastavenia, skontrolujte či sú údaje o Vás správne.</p>
 	<p><span id="reservation_reminders_span"><?php echo get_reservation_reminders(); ?></span> <label for="reservation_reminders_checkbox">Send me reservation reminders by email</label></p>
 
 	<p id="settings_message_p"></p>
@@ -137,25 +137,25 @@ else
 
 ?>
 
-	<h3>Your details</h3>
+	<h3>Údaje o Vás</h3>
 
-	<p class="smalltext_p">If you change your email, you must use the new one to log in. Password can be left blank to leave unchanged.</p>
+	<p class="smalltext_p">Ak si zmeníte prihlasovací email, pri nasledujúcom prihlásení musíte použiť už tento nový email. Heslo môžete ponechať prázdne pre zachovanie starého hesla.</p>
 
 	<form action="." id="user_details_form" autocomplete="off"><p>
 
 	<div id="user_details_div"><div>
-	<label for="user_name_input">Name:</label><br>
+	<label for="user_name_input">Číslo bytu:</label><br>
 	<input type="text" id="user_name_input" value="<?php echo $_SESSION['user_name']; ?>"><br><br>
 	<label for="user_email_input">Email:</label><br>
 	<input type="text" id="user_email_input" autocapitalize="off" value="<?php echo $_SESSION['user_email']; ?>">
 	</div><div>
-	<label for="user_password_input">Password:</label><br>
+	<label for="user_password_input">Heslo:</label><br>
 	<input type="password" id="user_password_input"><br><br>
-	<label for="user_password_confirm_input">Confirm password:</label><br>
+	<label for="user_password_confirm_input">Heslo znova:</label><br>
 	<input type="password" id="user_password_confirm_input">
 	</div></div>
 
-	<p><input type="submit" class="small_button blue_button" value="Update my details"></p>
+	<p><input type="submit" class="small_button blue_button" value="Uložiť údaje o mne"></p>
 
 	</p></form>
 
