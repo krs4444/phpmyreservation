cffaf1145b476f1a9545e389e2111b46a090c404
Kristian Pinke <kristian.pinke@live.com>
2019-01-30 23:58:55 +0100

    Current year to read reservation

--- phpmyreservation/functions.php
+++ phpmyreservation/functions.php
@@ -265,7 +265,8 @@ function highlight_day($day)
 
 function read_reservation($week, $day, $time)
 {
-	$query = mysql_query("SELECT * FROM " . global_mysql_reservations_table . " WHERE reservation_week='$week' AND reservation_day='$day' AND reservation_time='$time'")or die('<span class="error_span"><u>MySQL error:</u> ' . htmlspecialchars(mysql_error()) . '</span>');
+	$year = global_year;
+	$query = mysql_query("SELECT * FROM " . global_mysql_reservations_table . " WHERE reservation_week='$week' AND reservation_day='$day' AND reservation_year='$year' AND reservation_time='$time'")or die('<span class="error_span"><u>MySQL error:</u> ' . htmlspecialchars(mysql_error()) . '</span>');
 	$reservation = mysql_fetch_array($query);
 	return($reservation['reservation_user_name']);
 }
